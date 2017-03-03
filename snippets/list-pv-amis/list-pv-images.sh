7a80894481e7cc2cd78bb526e2ab3c40bdc5d8e7
Euan Kemp <euan.kemp@coreos.com>
2017-03-03 12:51:18 -0800

    Add a snippet to find PV container linux AMIs

---
+++ snippets/list-pv-amis/list-pv-images.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set -o nounset
+set -o pipefail
+
+# The account which publishes Container Linux AMIs
+IMAGE_OWNER=595879546273
+
+REGION="${1:?Please provide the AWS region to search as an argument}"
+
+# Get all running PV instances, including their image-id
+# The image-id is needed to figure out if it's container linux; unfortunately
+# there's no describe-instances filter for that
+instance_pairs=$(aws ec2 describe-instances \
+                     --output=json \
+                     --region=$REGION \
+                     --filters Name=virtualization-type,Values=paravirtual,Name=instance-state-name,Values=running | jq -c '.Reservations[].Instances[] | [.InstanceId, .ImageId]')
+
+# Poor man's hash-table to only have to describe each image-id once
+image_cache=""
+
+while read -r pair; do
+	image_id=$(jq -r -c '.[1]' <(echo "${pair}"))
+	is_containerlinux="0"
+
+	if grep "${image_id}=0" <(echo "${image_cache}") &>/dev/null; then
+		is_containerlinux="0"
+	elif grep "${image_id}=1" <(echo "${image_cache}") &>/dev/null; then
+		is_containerlinux="1"
+	else
+		is_containerlinux=$(aws ec2 describe-images --region=$REGION \
+			--image-ids="${image_id}" \
+			| jq ".Images[] | [select(.OwnerId == \"${IMAGE_OWNER}\")] | length")
+		image_cache="${image_cache}\n${image_id}=${is_containerlinux}"
+	fi
+		
+	if [[ "${is_containerlinux}" == "1" ]]; then
+		echo "${pair}" | jq -r -c '.[0]'
+	fi
+done <<< "${instance_pairs}"
