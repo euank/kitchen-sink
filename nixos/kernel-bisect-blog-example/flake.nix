9a671904e548caa9360fb605644e8ac56391533a
Euan Kemp <euank@euank.com>
2022-04-28 22:40:38 -0700

    nixos/kernel-bisect: Add qemu image based on repro config

--- nixos/kernel-bisect-blog-example/flake.nix
+++ nixos/kernel-bisect-blog-example/flake.nix
@@ -14,6 +14,7 @@
     pkgs = import nixpkgs {
       inherit system;
     };
+    evalConfig = import "${nixpkgs}/nixos/lib/eval-config.nix";
   in {
     nixosConfigurations = rec {
       # thinned down repro
@@ -26,6 +27,23 @@
           ./repro/configuration.nix
         ];
       };
+    };
+    qemuImage = (import "${nixpkgs}/nixos/lib/make-disk-image.nix") {
+      pkgs = pkgs;
+      lib = pkgs.lib;
+
+      diskSize = 8 * 1024;
+      format = "qcow2";
+      copyChannel = false;
+
+      config =
+        (evalConfig {
+          inherit system;
+          modules = [
+            (import ./repro/configuration.nix {inherit pkgs inputs;})
+          ];
+        })
+        .config;
     };
   };
 }
